bb88e504a718588e5097ae63af18d0b68cc3954d
luisl <llima@planerp.ec>
2024-01-05 08:13:05 -0500

    ADD: create home and media player

--- src/app/modules/tracks/pages/tracks-page/tracks-page.component.ts
+++ src/app/modules/tracks/pages/tracks-page/tracks-page.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { SectionGenericComponent } from '@shared/components/section-generic/section-generic.component';
 
 @Component({
   selector: 'app-tracks-page',
   standalone: true,
-  imports: [],
+  imports: [SectionGenericComponent],
   templateUrl: './tracks-page.component.html',
   styleUrl: './tracks-page.component.css'
 })
